968983a4232f08f59a3ed97a3941e24208107b5b
RandyCast21 <randycast21@gmail.com>
2025-09-22 16:55:23 -0600

    Add a clean module, and a clean function in the order modules

--- src/initialPage.js
+++ src/initialPage.js
@@ -1,4 +1,5 @@
 function initialPage() {
+    cleanTab();
     let contentBox = document.querySelector("#content")
 
     let headline = document.createElement("div")
@@ -17,35 +18,18 @@ function initialPage() {
 
 }
 
-function homeTab() {
-    let contentBoxHome = document.querySelector("#content")
-
-    let headline = document.createElement("div")
-    headline.id = "headline"
-
-    let headerPizzaPlace = document.createElement("header")
-    headerPizzaPlace.textContent = "Freddy Pizza Place";
-
-    let wonderfulText = document.createElement("div")
-    wonderfulText.textContent = "It's the best pizza place on the planet"
-
-    contentBox.appendChild(headline)
-    headline.appendChild(headerPizzaPlace)
-
-    contentBox.appendChild(wonderfulText)
-}
-
 function menuTab() {
-    let contentBoxMenu = document.querySelector('#content')
+    cleanTab();
+    let contentBoxMenu = document.querySelector("#content")
 
     let headline = document.createElement("header")
     headline.textContent = "Menú"
 
     let pizzaMenu = document.createElement("div")
-    headline.textContent = "Pizza"
+    pizzaMenu.textContent = "Pizza"
 
     let sodaMenu = document.createElement("div")
-    headline.textContent = "Soda"
+    sodaMenu.textContent = "Soda"
 
     contentBoxMenu.appendChild(headline)
     contentBoxMenu.appendChild(pizzaMenu)
@@ -53,10 +37,22 @@ function menuTab() {
 }
 
 function contactTab() {
+    cleanTab();
     let contentBox = document.querySelector("#content")
 
     let contact = document.createElement("div")
     contact.textContent = "Contacto"
+
+    contentBox.appendChild(contact)
+}
+
+function cleanTab() {
+    let contentBox = document.querySelector("#content")
+
+    while (contentBox.lastElementChild) {
+        contentBox.removeChild(contentBox.lastElementChild);
+    }
+
 }
 
 export { initialPage, menuTab, contactTab }
