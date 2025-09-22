e9a827918d9d99308dcc0f3027776456b94fbb95
RandyCast21 <randycast21@gmail.com>
2025-09-22 17:24:39 -0600

    Add events and functions to the corresponding buttons to display the tab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import './style.css'
+import { contactTab, initialPage, menuTab } from './initialPage.js'
+
+//initialPage();
+//menuTab();
+//contactTab();
+
+let homeButton = document.querySelector("#homeButton")
+let menuButton = document.querySelector("#menuButton")
+let aboutButton = document.querySelector("#aboutButton")
+
+homeButton.addEventListener('click', () => {
+    initialPage();
+})
+
+menuButton.addEventListener('click', () => {
+    menuTab();
+})
+
+aboutButton.addEventListener('click', () => {
+    contactTab();
+})
+
+
 
 console.log("Hola")
